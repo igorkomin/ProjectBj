5d31acf1a2d3510c6bbef216c44480852d65f391
Igor Komin <ik.kture@gmail.com>
2018-07-05 16:07:04 +0300

    Fixed total value calculation, minor changes

--- ProjectBj.BLL/BusinessModels/GameManager.cs
+++ ProjectBj.BLL/BusinessModels/GameManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ProjectBj.Entities;
 using ProjectBj.DAL.Repositories;
+using ProjectBj.DAL.Utility;
 using System.Diagnostics;
 
 namespace ProjectBj.BLL.BusinessModels
@@ -24,16 +25,21 @@ namespace ProjectBj.BLL.BusinessModels
             _players = new List<Player>();
         }
 
-        public void AddPlayer(Player newPlayer)
+        public Player GetDealer()
         {
-            _database.Players.Create(newPlayer);
-            _players.Add(newPlayer);
+            return _dealer;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            _database.Players.Create(player);
+            _players.Add(player);
         }
 
         public void DealFirstTwoCards()
         {
-            //_deck.DealCard(_dealer, true);
-            //_deck.DealCard(_dealer, true);
+            _deck.DealCard(_dealer, true);
+            _deck.DealCard(_dealer, true);
 
             foreach(var player in _players)
             {
@@ -46,46 +52,43 @@ namespace ProjectBj.BLL.BusinessModels
         public int GetHandTotal(List<Card> cards)
         {
             int totalValue = 0;
-            bool hasAce = false;
+            int aceCount = 0;
 
             foreach (var card in cards)
             {
                 if(card.Rank == Values.ACE)
                 {
-                    if (!hasAce)
-                        hasAce = true;
-                    if (hasAce && totalValue > 21)
-                        totalValue -= 10;
+                    aceCount++;
                 }
                 totalValue += card.Value;
             }
 
-            return totalValue;
+            return totalValue > 21 ? totalValue - aceCount * 10 : totalValue;
         }
 
         public List<Player> GetPlayers()
         {
-            return _players;
+            return DatabaseHelper.SyncPlayers(_players);
         }
 
         public List<Card> GetCards(Player player)
-        {
-            return player.Cards.ToList();
+        { 
+            return DatabaseHelper.GetPlayerById(player.Id).Cards.ToList();
         }
 
-        public bool IsBlackjack(List <Card> cards)
+        public bool IsBlackjack(int handTotal)
         {
-            return GetHandTotal(cards) == 21 ? true : false;
+            return handTotal == 21 ? true : false;
         }
 
-        public bool IsBust(List <Card> cards)
+        public bool IsBust(int handTotal)
         {
-            return GetHandTotal(cards) > 21 ? true : false;
+            return handTotal > 21 ? true : false;
         }
 
         public void FillDealerHand()
         {
-            while(GetHandTotal(GetCards(_dealer)) < 17)
+            while(GetHandTotal(_dealer.Cards.ToList()) < 17)
             {
                 _deck.DealCard(_dealer, true);
             }
