36b49fc0fce711707b1a60acdb6cc596f405bfdc
Igor Komin <ik.kture@gmail.com>
2018-07-09 15:22:04 +0300

    deleted deck fill

--- ProjectBj.DAL/EF/BjDbInitializer.cs
+++ ProjectBj.DAL/EF/BjDbInitializer.cs
@@ -11,22 +11,7 @@ namespace ProjectBj.DAL.EF
     public class BjDbInitializer : DropCreateDatabaseIfModelChanges<BjContext>
     {
         protected override void Seed(BjContext db)
-        {
-            List<Card> cards = new List<Card>();
-            int minCardValue = 2;
-            int maxCardValue = 11;
-
-            foreach (var suit in Enum.GetValues(typeof(Values.Suits)))
-            {
-                for (int i = minCardValue; i < maxCardValue; i++)
-                {
-                    db.Cards.Add(new Card(i.ToString(), suit.ToString()));
-                }
-                foreach(var face in Enum.GetValues(typeof(Values.Faces)))
-                {
-                    db.Cards.Add(new Card(face.ToString(), suit.ToString()));
-                }
-            }
+        {     
         }
     }
 }
