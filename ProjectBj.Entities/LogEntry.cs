00d79fc470c06b8950ab830c0e09e0ddd520aa3d
Igor Komin <ik.kture@gmail.com>
2018-07-23 12:00:57 +0300

    added LogEntry entity

---
+++ ProjectBj.Entities/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBj.Entities
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+    }
+}
