ff7b0f8861e0659ac400c53eed48c5654cd6f301
Igor Komin <ik.kture@gmail.com>
2018-07-23 13:00:45 +0300

    initial commit

---
+++ ProjectBj.Service/LogService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectBj.Entities;
+using ProjectBj.DAL.Repositories;
+
+namespace ProjectBj.Service
+{
+    public static class LogService
+    {
+        private static LogRepository _logRepository;
+
+        static LogService()
+        {
+            _logRepository = new LogRepository();
+        }
+
+        public static void PushLogEntry(LogEntry entry)
+        {
+            _logRepository.CreateEntry(entry);
+        }
+
+        public static LogEntry PullLogEntry(int id)
+        {
+            LogEntry entry = _logRepository.GetEntry(id);
+            return entry;
+        }
+
+        public static List<LogEntry> PullLogs()
+        {
+            List<LogEntry> entries = _logRepository.GetAllLogs().ToList();
+            return entries;
+        }
+
+        public static void DeleteLogEntry(LogEntry entry)
+        {
+            _logRepository.DeleteEntry(entry.Id);
+        }
+    }
+}
