0cf4a3030f0abb157dfa9a269c22ccd2a9111b63
Igor Komin <ik.kture@gmail.com>
2018-07-23 17:02:50 +0300

    moved AppStrings and Values to Common project

--- ProjectBj.Common/AppStrings.cs
+++ ProjectBj.Common/AppStrings.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectBj.Configuration
+namespace ProjectBj.Common
 {
     public static class AppStrings
     {
--- ProjectBj.Common/Values.cs
+++ ProjectBj.Common/Values.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectBj.Configuration
+namespace ProjectBj.Common
 {
     public static class Values
     {
