2f4467c9db8e451e93fce3a59b49899035b0bf96
Igor Komin <ik.kture@gmail.com>
2018-07-24 09:07:31 +0300

    renamed GameSession class to GameSessionPlayer

---
+++ ProjectBj.Entities/GameSessionPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBj.Entities
+{
+    class GameSessionPlayer
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public DateTime TimeJoined { get; set; }
+        public int Score { get; set; }
+    }
+}
