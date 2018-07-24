f12b53e5dc1366da075be6769bb27a94e21f3662
Igor Komin <ik.kture@gmail.com>
2018-07-24 18:02:19 +0300

    added SessionId property

--- ProjectBj.Entities/LogEntry.cs
+++ ProjectBj.Entities/LogEntry.cs
@@ -9,6 +9,7 @@ namespace ProjectBj.Entities
     public class LogEntry
     {
         public int Id { get; set; }
+        public int SessionId { get; set; }
         public DateTime Time { get; set; }
         public string Message { get; set; }
     }
