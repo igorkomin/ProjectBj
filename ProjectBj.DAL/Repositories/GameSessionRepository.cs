863c7c79dd99eb410d943a1dcc1ef201441e1206
Igor Komin <ik.kture@gmail.com>
2018-07-24 18:00:24 +0300

    initial commit

---
+++ ProjectBj.DAL/Repositories/GameSessionRepository.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using ProjectBj.Common;
+using ProjectBj.Common.ExceptionHandlers;
+using ProjectBj.Entities;
+
+namespace ProjectBj.DAL.Repositories
+{
+    public class GameSessionRepository
+    {
+        public GameSession Create(GameSession session)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                {
+                    var sqlQuery = "INSERT INTO GameSessions (TimeCreated) " +
+                                   "VALUES (@TimeCreated);" +
+                                   "SELECT CAST(SCOPE_IDENTITY() as int)";
+                    int sessionId = db.Query<int>(sqlQuery, session).FirstOrDefault();
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new DataSourceException(exception.Message, exception);
+            }
+            return session;
+        }
+
+        public void Update(GameSession session)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                {
+                    var sqlQuery = "UPDATE GameSessions " +
+                                   "SET IsOpen = @IsOpen " +
+                                   "WHERE Id = @Id";
+                    db.Execute(sqlQuery, session);
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new DataSourceException(exception.Message, exception);
+            }
+        }
+
+        public void Delete(GameSession session)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                {
+                    var sqlQuery = "DELETE FROM GameSessions " +
+                                   "WHERE Id = @Id";
+                    db.Execute(sqlQuery, session);
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new DataSourceException(exception.Message, exception);
+            }
+        }
+
+        public void AddPlayer(GameSession session, Player player)
+        {
+            try
+            {
+                GameSessionPlayer gameSessionPlayer = new GameSessionPlayer() { SessionId = session.Id, PlayerId = player.Id };
+                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                {
+                    var sqlQuery = "INSERT INTO GameSessionPlayers (SessionId, PlayerId, TimeJoined) " +
+                                   "VALUES (@SessionId, @PlayerId, @TimeJoined)";
+                    db.Execute(sqlQuery, gameSessionPlayer);
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new DataSourceException(exception.Message, exception);
+            }
+        }
+
+        public ICollection<Player> GetSessionPlayers(GameSession session)
+        {
+            List<Player> players;
+            try
+            {
+                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                {
+                    var sqlQuery = "SELECT p.* FROM GameSessionPlayers gsp " +
+                                   "JOIN Players p ON ( gsp.PlayerId = p.Id ) " +
+                                   "JOIN GameSessions gs ON ( gsp.SessionId = gs.Id ) " +
+                                   "WHERE gs.Id = @Id";
+                    players = db.Query<Player>(sqlQuery, session).AsList();
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new DataSourceException(exception.Message, exception);
+            }
+            return players;
+        }
+
+        public ICollection<GameSession> GetPlayerSessions(Player player)
+        {
+            List<GameSession> sessions;
+            try
+            {
+                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                {
+                    var sqlQuery = "SELECT gs.* FROM GameSessionPlayers gsp " +
+                                   "JOIN Players p ON ( gsp.PlayerId = p.Id ) " +
+                                   "JOIN GameSessions gs ON ( gsp.PlayerId = gs.Id ) " +
+                                   "WHERE p.Id = @Id";
+
+                    sessions = db.Query<GameSession>(sqlQuery, player).AsList();
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new DataSourceException(exception.Message, exception);
+            }
+            return sessions;
+        }
+
+        public GameSession GetCurrentSession(Player player)
+        {
+            GameSession session;
+
+            try
+            {
+                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                {
+                    var sqlQuery = "SELECT gs.* FROM GameSessionPlayers gsp " +
+                                   "JOIN Players p ON ( gsp.PlayerId = p.Id ) " +
+                                   "JOIN GameSessions gs ON ( gsp.PlayerId = gs.Id ) " +
+                                   "WHERE gs.IsOpen = 1 AND p.Id = @Id";
+
+                    session = db.Query<GameSession>(sqlQuery, player).FirstOrDefault();
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw new DataSourceException(exception.Message, exception);
+            }
+            return session;
+        }
+    }
+}
