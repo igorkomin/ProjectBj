c0fbea58e753c611af61178955275d28733f47e0
Igor Komin <ik.kture@gmail.com>
2018-07-25 14:08:09 +0300

    initial commit

---
+++ ProjectBj.DAL/DatabaseConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectBj.DAL
+{
+    internal class DatabaseConfiguration
+    {
+        public static readonly string Server = "localhost";
+        public static readonly string Database = "blackjack";
+        public static readonly string ConnectionString = $"Server={Server};Database={Database};Trusted_Connection=True;";
+    }
+}
---
+++ ProjectBj.Service/Helpers/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectBj.Service.Helpers
+{
+    public class StringHelper
+    {
+        #region Names
+        public static readonly string DealerName = "Dealer";
+        public static readonly string BotName = "Bot"; 
+        #endregion
+        #region Messages
+        public static string PlayerScore(string playerName, int score)
+        {
+            return $"{playerName} has total score: {score}";
+        }
+        public static string PlayerTakesCard(string playerName, string cardRank, string cardSuit)
+        {
+            return $"{playerName} takes {cardRank} of {cardSuit}";
+        }
+        public static string PlayerHits(string playerName)
+        {
+            return $"{playerName} hits";
+        }
+        public static string PlayerStays(string playerName)
+        {
+            return $"{playerName} stays";
+        } 
+        #endregion
+    }
+}
