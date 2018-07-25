5cf274ddc6b2fa412014977ddb60883859149246
Igor Komin <ik.kture@gmail.com>
2018-07-25 17:43:55 +0300

    refactor

--- ProjectBj.DAL/Repositories/GameSessionRepository.cs
+++ ProjectBj.DAL/Repositories/GameSessionRepository.cs
@@ -6,19 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
-using ProjectBj.Common;
-using ProjectBj.Common.ExceptionHandlers;
 using ProjectBj.Entities;
+using ProjectBj.DAL.Interfaces;
+using ProjectBj.DAL.ExceptionHandlers;
 
 namespace ProjectBj.DAL.Repositories
 {
-    public class GameSessionRepository
+    public class GameSessionRepository : IGameSessionRepository
     {
         public GameSession Create(GameSession session)
         {
             try
             {
-                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                using (IDbConnection db = new SqlConnection(DatabaseConfiguration.ConnectionString))
                 {
                     var sqlQuery = "INSERT INTO GameSessions (TimeCreated) " +
                                    "VALUES (@TimeCreated);" +
@@ -37,7 +37,7 @@ namespace ProjectBj.DAL.Repositories
         {
             try
             {
-                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                using (IDbConnection db = new SqlConnection(DatabaseConfiguration.ConnectionString))
                 {
                     var sqlQuery = "UPDATE GameSessions " +
                                    "SET IsOpen = @IsOpen " +
@@ -55,7 +55,7 @@ namespace ProjectBj.DAL.Repositories
         {
             try
             {
-                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                using (IDbConnection db = new SqlConnection(DatabaseConfiguration.ConnectionString))
                 {
                     var sqlQuery = "DELETE FROM GameSessions " +
                                    "WHERE Id = @Id";
@@ -73,7 +73,7 @@ namespace ProjectBj.DAL.Repositories
             try
             {
                 GameSessionPlayer gameSessionPlayer = new GameSessionPlayer() { SessionId = session.Id, PlayerId = player.Id };
-                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                using (IDbConnection db = new SqlConnection(DatabaseConfiguration.ConnectionString))
                 {
                     var sqlQuery = "INSERT INTO GameSessionPlayers (SessionId, PlayerId, TimeJoined) " +
                                    "VALUES (@SessionId, @PlayerId, @TimeJoined)";
@@ -91,20 +91,20 @@ namespace ProjectBj.DAL.Repositories
             List<Player> players;
             try
             {
-                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                using (IDbConnection db = new SqlConnection(DatabaseConfiguration.ConnectionString))
                 {
                     var sqlQuery = "SELECT p.* FROM GameSessionPlayers gsp " +
                                    "JOIN Players p ON ( gsp.PlayerId = p.Id ) " +
                                    "JOIN GameSessions gs ON ( gsp.SessionId = gs.Id ) " +
                                    "WHERE gs.Id = @Id";
                     players = db.Query<Player>(sqlQuery, session).AsList();
+                    return players;
                 }
             }
             catch (SqlException exception)
             {
                 throw new DataSourceException(exception.Message, exception);
             }
-            return players;
         }
 
         public ICollection<GameSession> GetPlayerSessions(Player player)
@@ -112,7 +112,7 @@ namespace ProjectBj.DAL.Repositories
             List<GameSession> sessions;
             try
             {
-                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                using (IDbConnection db = new SqlConnection(DatabaseConfiguration.ConnectionString))
                 {
                     var sqlQuery = "SELECT gs.* FROM GameSessionPlayers gsp " +
                                    "JOIN Players p ON ( gsp.PlayerId = p.Id ) " +
@@ -120,13 +120,13 @@ namespace ProjectBj.DAL.Repositories
                                    "WHERE p.Id = @Id";
 
                     sessions = db.Query<GameSession>(sqlQuery, player).AsList();
+                    return sessions;
                 }
             }
             catch (SqlException exception)
             {
                 throw new DataSourceException(exception.Message, exception);
             }
-            return sessions;
         }
 
         public GameSession GetCurrentSession(Player player)
@@ -135,7 +135,7 @@ namespace ProjectBj.DAL.Repositories
 
             try
             {
-                using (IDbConnection db = new SqlConnection(AppStrings.ConnectionString))
+                using (IDbConnection db = new SqlConnection(DatabaseConfiguration.ConnectionString))
                 {
                     var sqlQuery = "SELECT gs.* FROM GameSessionPlayers gsp " +
                                    "JOIN Players p ON ( gsp.PlayerId = p.Id ) " +
@@ -143,13 +143,13 @@ namespace ProjectBj.DAL.Repositories
                                    "WHERE gs.IsOpen = 1 AND p.Id = @Id";
 
                     session = db.Query<GameSession>(sqlQuery, player).FirstOrDefault();
+                    return session;
                 }
             }
             catch (SqlException exception)
             {
                 throw new DataSourceException(exception.Message, exception);
             }
-            return session;
         }
     }
 }
