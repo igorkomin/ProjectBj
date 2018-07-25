e64e870425690a4ec3723384250d181675fe8e37
Igor Komin <ik.kture@gmail.com>
2018-07-25 13:01:34 +0300

    initial commit

---
+++ ProjectBj.Service/Helpers/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBj.Service.Helpers
+{
+    public static class EnumHelper
+    {
+        public static string GetEnumDescription(Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            var attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes[0].Description;
+        }
+    }
+}
