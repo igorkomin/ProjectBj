50aab34a84153d2d528532fdc17a54ae6715c26f
Igor Komin <ik.kture@gmail.com>
2018-08-01 17:45:26 +0300

    Initial commit

---
+++ ProjectBj.Service/Utility/AutofacConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using ProjectBj.Service;
+using ProjectBj.Service.Interfaces;
+
+namespace ProjectBj.Service.Utility
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DeckService>().As<IDeckService>();
+            builder.RegisterType<GameService>().As<IGameService>();
+            builder.RegisterType<LogService>().As<ILogService>();
+            builder.RegisterType<PlayerService>().As<IPlayerService>();
+            builder.RegisterType<SessionService>().As<ISessionService>();
+            ContainerProvider.Container = builder.Build();
+        }
+    }
+}
---
+++ ProjectBj.Service/Utility/ContainerProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace ProjectBj.Service.Utility
+{
+    public static class ContainerProvider
+    {
+        public static IContainer Container { get; set; }
+    }
+}
