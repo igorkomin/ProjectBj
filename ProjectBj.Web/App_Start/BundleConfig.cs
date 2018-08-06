c895de2478e351731c9d2e30d2652abccd69352e
Igor Komin <ik.kture@gmail.com>
2018-08-06 18:00:55 +0300

    Refactor bundles

--- ProjectBj.Web/App_Start/BundleConfig.cs
+++ ProjectBj.Web/App_Start/BundleConfig.cs
@@ -12,15 +12,30 @@ namespace ProjectBj.Web.App_Start
         {
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                         "~/Scripts/bootstrap.js",
-                        "~/Scripts/jquery-3.3.1.js",
-                        "~/Scripts/main.js",
                         "~/Scripts/slider.js",
-                        "~/Scripts/slider-bots.js"));
+                        "~/Scripts/slider-bots.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.3.1.js",
+                        "~/Scripts/jquery-ui.js",
+                        "~/Scripts/jquery.cookie.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/login").Include(
+                        "~/Scripts/login.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                        "~/Scripts/main.js"
+                        ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
                         "~/Content/style.css",
-                        "~/Content/slider.css"));
+                        "~/Content/jquery-ui.css",
+                        "~/Content/slider.css"
+                        ));
         }
     }
 }
