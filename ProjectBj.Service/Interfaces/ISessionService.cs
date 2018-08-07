99fe89c62e042d25657a304a219d0b4bd7dec445
Igor Komin <ik.kture@gmail.com>
2018-08-07 14:27:35 +0300

    Change ISessionService visibility to public

--- ProjectBj.Service/Interfaces/ISessionService.cs
+++ ProjectBj.Service/Interfaces/ISessionService.cs
@@ -7,7 +7,7 @@ using ProjectBj.Entities;
 
 namespace ProjectBj.Service.Interfaces
 {
-    interface ISessionService
+    public interface ISessionService
     {
         Task<int> GetSessionByPlayerId(int playerId);
         Task<GameSession> GetSessionById(int id);
