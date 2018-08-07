71bacf508e1335548a96f0fe85c615d0e812a31f
Igor Komin <ik.kture@gmail.com>
2018-08-07 11:03:39 +0300

    Initial commit

---
+++ ProjectBj.ViewModels/Game/HandViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBj.ViewModels.Game
+{
+    public class HandViewModel
+    {
+        public List<CardViewModel> Cards { get; set; }
+        public int Score { get; set; }
+    }
+}
