d8a6adb6a997cc7adf27e834ebe4630d95ddbab4
Igor Komin <ik.kture@gmail.com>
2018-08-15 14:20:48 +0300

    Initial commit

---
+++ ProjectBj.Web/src/app/components/game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './../views/game.view.html',
+  styleUrls: ['./../styles/game.style.css']
+})
+export class GameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ProjectBj.Web/src/app/components/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './../views/login.view.html',
+  styleUrls: ['./../styles/login.style.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
