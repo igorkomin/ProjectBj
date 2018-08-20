f7a6074a229a2b0751f801eece9fc690576d22fd
Igor Komin <ik.kture@gmail.com>
2018-08-20 14:49:35 +0300

    Refactor LogEntry
    Add key attribute to Id

--- ProjectBj.Entities/LogEntry.cs
+++ ProjectBj.Entities/LogEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace ProjectBj.Entities
 {
     public class LogEntry
     {
+        [Key]
         public int Id { get; set; }
+
         public int SessionId { get; set; }
         public DateTime Time { get; set; }
         public string Message { get; set; }
