c8e5a1cefeca1e344bfdbbc4c9ba984cc8bbb8fb
Igor Komin <ik.kture@gmail.com>
2018-08-20 17:40:20 +0300

    Revert "Refactor LogEntry"
    This reverts commit f7a6074a229a2b0751f801eece9fc690576d22fd.

--- ProjectBj.Entities/LogEntry.cs
+++ ProjectBj.Entities/LogEntry.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +8,7 @@ namespace ProjectBj.Entities
 {
     public class LogEntry
     {
-        [Key]
         public int Id { get; set; }
-
         public int SessionId { get; set; }
         public DateTime Time { get; set; }
         public string Message { get; set; }
