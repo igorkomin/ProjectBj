e5c4ea9237d42f73fdbcfbfd18308ad81a583cc0
Igor Komin <ik.kture@gmail.com>
2018-08-20 14:49:49 +0300

    Refactor Player
    Add key attribute to Id

--- ProjectBj.Entities/Player.cs
+++ ProjectBj.Entities/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace ProjectBj.Entities
 {
     public class Player
     {
+        [Key]
         public int Id { get; set; }
+
         public string Name { get; set; }
         public bool IsHuman { get; set; }
         public int Balance { get; set; }
