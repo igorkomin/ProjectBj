57212893eb5aa705b98cadcb4016b3266593fbd0
Igor Komin <ik.kture@gmail.com>
2018-08-20 14:49:17 +0300

    Refactor GameSession
    Add key attribute to Id

--- ProjectBj.Entities/GameSession.cs
+++ ProjectBj.Entities/GameSession.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,10 @@ namespace ProjectBj.Entities
         {
             IsOpen = true;
         }
+
+        [Key]
         public int Id { get; set; }
+
         public bool IsOpen { get; set; }
         public DateTime TimeCreated { get; set; }
     }
