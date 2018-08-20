4d2d570f3cb6a3677098cedcf10a102d8b346cf5
Igor Komin <ik.kture@gmail.com>
2018-08-20 16:47:47 +0300

    Initial commit

---
+++ ProjectBj.ViewModels/Game/LogEntryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBj.ViewModels.Game
+{
+    public class LogEntryViewModel
+    {
+        public int SessionId { get; set; }
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+    }
+}
