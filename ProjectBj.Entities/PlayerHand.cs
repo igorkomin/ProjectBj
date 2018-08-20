4c332791a3b1571dbca4c9b9a0682a36374c296a
Igor Komin <ik.kture@gmail.com>
2018-08-20 14:50:08 +0300

    Refactor PlayerHand
    Add key attribute to Id

--- ProjectBj.Entities/PlayerHand.cs
+++ ProjectBj.Entities/PlayerHand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace ProjectBj.Entities
 {
     public class PlayerHand
     {
+        [Key]
         public int Id { get; set; }
+
         public int PlayerId { get; set; }
         public int CardId { get; set; }
         public int SessionId { get; set; }
