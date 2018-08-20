3b070706386fdad9ee4f0b84a91875c09104c2f4
Igor Komin <ik.kture@gmail.com>
2018-08-20 11:52:04 +0300

    Delete debug code

--- ProjectBj.Web/src/app/components/game.component.ts
+++ ProjectBj.Web/src/app/components/game.component.ts
@@ -31,33 +31,9 @@ export class GameComponent implements OnInit {
                 this.playerName = params['name'];
                 this.botsNumber = params['bots'];
             });
-        let data = this.postData().subscribe({
-            next(response) {
-                console.log(response.playerName);
-                console.log(response.botsNumber);
-            },
-            error(exception) {
-                console.error(exception.error.exceptionMessage);
-            }
-        });
-        /*let array = [0, 1, 2];
-        array.forEach((item, index) => {
-            document.getElementById("name-" + index).innerText = index.toString();
-        });*/
     }
 
     updateSliderValue(value: number): void {
         this.sliderValue = value;
-    }
-
-    postData(): Observable<any> {
-        let url = "/api/main/debug";
-        let options = {
-            headers: new HttpHeaders({ 'Content-Type' : 'application/json' })
-        };
-        return this.http.post(url, {
-            PlayerName: this.playerName,
-            BotsNumber: this.botsNumber
-        }, options);
     }
 }
