9f408a1082f0ac466863e417c3f2b2328c651917
Igor Komin <ik.kture@gmail.com>
2018-08-21 16:58:07 +0300

    Add descriptions

--- ProjectBj.BusinessLogic/Enums/CardSuits.cs
+++ ProjectBj.BusinessLogic/Enums/CardSuits.cs
@@ -11,9 +11,13 @@ namespace ProjectBj.BusinessLogic.Enums
     {
         public enum Suit
         {
+            [Description("spades")]
             Spades = 0,
+            [Description("clubs")]
             Clubs = 1,
+            [Description("hearts")]
             Hearts = 2,
+            [Description("diams")]
             Diamonds = 3
         }
     }
