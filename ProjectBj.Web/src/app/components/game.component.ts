4023ae59ff90ec9e84f127205b38cd96ffb306f8
Igor Komin <ik.kture@gmail.com>
2018-08-21 17:58:47 +0300

    Add getGame method

--- ProjectBj.Web/src/app/components/game.component.ts
+++ ProjectBj.Web/src/app/components/game.component.ts
@@ -3,11 +3,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from '../../../node_modules/rxjs';
 
+import { GameService } from '../services/game.service';
+import { Settings } from '../models/settings.model';
+
 @Component({
     selector: 'app-game',
     templateUrl: './../views/game.view.html',
     styleUrls: [
-        './../styles/game.style.css',
         './../styles/common/bootstrap.css',
         './../styles/common/slider.css',
         './../styles/common/cards.css'
@@ -15,26 +17,39 @@ import { Observable } from '../../../node_modules/rxjs';
 })
 export class GameComponent implements OnInit {
 
-    playerName: string;
-    botsNumber: number;
+    gameSettings: Settings;
     sliderValue: number = 50;
+    sessionId: number;
+    playerId: number;
+    game: any;
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
-        private http: HttpClient
+        private http: HttpClient,
+        private gameService: GameService
     ) { }
 
     ngOnInit() {
         this.route
             .queryParams
             .subscribe(params => {
-                this.playerName = params['name'];
-                this.botsNumber = params['bots'];
+                this.gameSettings.playerName = params['name'];
+                this.gameSettings.botsNumber = params['bots'];
             });
     }
 
     updateSliderValue(value: number): void {
         this.sliderValue = value;
+    }
+    getGame(): void {
+        this.gameService.getGameViewModel(this.gameSettings).subscribe({
+            next(game) {
+                this.game = game;
+            },
+            error(exception) {
+                console.error(exception.error.exceptionMessage);
+            }
+        });
     }
 }
