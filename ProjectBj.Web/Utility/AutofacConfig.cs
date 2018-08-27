db9cd889cef3e8937c51fda7ecebc79e6a4b4b63
Igor Komin <ik.kture@gmail.com>
2018-08-27 11:39:14 +0300

    Refactor AutofacConfig
    Remove unnecessary usings

--- ProjectBj.Web/Utility/AutofacConfig.cs
+++ ProjectBj.Web/Utility/AutofacConfig.cs
@@ -1,19 +1,7 @@
 ﻿using Autofac;
 using Autofac.Integration.WebApi;
-using Autofac.Integration.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Compilation;
 using System.Web.Http;
-using System.Web.Mvc;
-using ProjectBj.BusinessLogic;
-using ProjectBj.BusinessLogic.Interfaces;
-using ProjectBj.BusinessLogic.Providers;
-using ProjectBj.BusinessLogic.Services;
 using ProjectBj.BusinessLogic.Utility;
 using System.Web.Configuration;
 
