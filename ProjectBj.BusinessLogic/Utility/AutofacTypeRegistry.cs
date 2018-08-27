950bf05495d94d0a40da35af1a9dcc27ba5d626d
Igor Komin <ik.kture@gmail.com>
2018-08-27 11:23:02 +0300

    Initial commit
    Add class to register all types is Autofac

---
+++ ProjectBj.BusinessLogic/Utility/AutofacTypeRegistry.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectBj.BusinessLogic.Interfaces;
+using ProjectBj.BusinessLogic.Providers;
+using ProjectBj.BusinessLogic.Services;
+using ProjectBj.DataAccess.Interfaces;
+using ProjectBj.DataAccess.Repositories;
+
+namespace ProjectBj.BusinessLogic.Utility
+{
+    public class AutofacTypeRegistry
+    {
+        public static ContainerBuilder RegisterTypes(ContainerBuilder builder, string connectionString)
+        {
+            //string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            builder.RegisterType<CardRepository>().As<ICardRepository>()
+                .WithParameter("connectionString", connectionString);
+            builder.RegisterType<GameSessionRepository>().As<IGameSessionRepository>()
+                .WithParameter("connectionString", connectionString);
+            builder.RegisterType<LogRepository>().As<ILogRepository>()
+                .WithParameter("connectionString", connectionString);
+            builder.RegisterType<PlayerRepository>().As<IPlayerRepository>()
+                .WithParameter("connectionString", connectionString);
+
+            builder.RegisterType<DeckProvider>().As<IDeckProvider>();
+            builder.RegisterType<GameProvider>().As<IGameProvider>();
+            builder.RegisterType<LogProvider>().As<ILogProvider>();
+            builder.RegisterType<PlayerProvider>().As<IPlayerProvider>();
+            builder.RegisterType<SessionProvider>().As<ISessionProvider>();
+            builder.RegisterType<GameService>().As<IGameService>();
+
+            return builder;
+        }
+    }
+}
