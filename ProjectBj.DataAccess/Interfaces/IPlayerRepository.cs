b9d1b8c9ce567819639009bc3b08b5f7ec8ad3df
Igor Komin <ik.kture@gmail.com>
2018-08-27 11:25:53 +0300

    Refactor IPlayerRepository
    Make interface public, Add methods: GetSessionBots, DeleteNonHumanPlayers

--- ProjectBj.DataAccess/Interfaces/IPlayerRepository.cs
+++ ProjectBj.DataAccess/Interfaces/IPlayerRepository.cs
@@ -7,18 +7,20 @@ using ProjectBj.Entities;
 
 namespace ProjectBj.DataAccess.Interfaces
 {
-    interface IPlayerRepository
+    public interface IPlayerRepository
     {
         Task<Player> CreateOne(Player player);
         Task<Player> GetById(int id);
         Task<ICollection<Player>> CreateMany(ICollection<Player> players);
         Task<ICollection<Player>> FindPlayers(string name);
         Task<ICollection<Player>> GetAllPlayers();
+        Task<ICollection<Player>> GetSessionBots(int sessionId);
         Task<ICollection<Card>> GetCards(Player player, int sessionId);
         Task Delete(Player player);
         Task Update(Player player);
         Task AddCard(Player player, Card card, int sessionId);
         Task DeleteCards(Player player);
         Task DeletePlayersByName(string name);
+        Task DeleteNonHumanPlayers();
     }
 }
