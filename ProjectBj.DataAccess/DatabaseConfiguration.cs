a0e9610d24ece1414d6ab429ad62600b54e57392
Igor Komin <ik.kture@gmail.com>
2018-08-27 11:28:22 +0300

    Delete unused class

--- ProjectBj.DataAccess/DatabaseConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjectBj.DataAccess
-{
-    internal static class DatabaseConfiguration
-    {
-        public static readonly string Server = "localhost";
-        public static readonly string Database = "blackjack";
-        public static readonly string ConnectionString = $"Server={Server};Database={Database};Trusted_Connection=True;";
-    }
-}
