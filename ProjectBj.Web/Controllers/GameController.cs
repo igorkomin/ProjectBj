36fb2be709246ef64a1806b159bba7ab55d0f781
Igor Komin <ik.kture@gmail.com>
2018-08-28 16:51:24 +0300

    Delete reference to Logger

--- ProjectBj.Web/Controllers/GameController.cs
+++ ProjectBj.Web/Controllers/GameController.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using ProjectBj.Logger;
 using ProjectBj.BusinessLogic.Interfaces;
 using ProjectBj.ViewModels;
 using ProjectBj.ViewModels.Game;
