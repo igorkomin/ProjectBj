30037b5883f95c8a1ce156b449731595bcdc9e8b
Igor Komin <ik.kture@gmail.com>
2018-08-29 11:07:20 +0300

    Delete GameProvider registration

--- ProjectBj.BusinessLogic/Utility/AutofacProviderTypeRegistry.cs
+++ ProjectBj.BusinessLogic/Utility/AutofacProviderTypeRegistry.cs
@@ -15,7 +15,6 @@ namespace ProjectBj.BusinessLogic.Utility
         public static ContainerBuilder RegisterTypes(ContainerBuilder builder, string connectionString)
         {
             builder.RegisterType<DeckProvider>().As<IDeckProvider>();
-            builder.RegisterType<GameProvider>().As<IGameProvider>();
             builder.RegisterType<LogProvider>().As<ILogProvider>();
             builder.RegisterType<PlayerProvider>().As<IPlayerProvider>();
             builder.RegisterType<SessionProvider>().As<ISessionProvider>();
