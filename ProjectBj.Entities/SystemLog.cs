df1a6da265ef58fbce769f80457354f9667479c4
Igor Komin <ik.kture@gmail.com>
2018-08-30 09:18:22 +0300

    Initial commit

---
+++ ProjectBj.Entities/SystemLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBj.Entities
+{
+    public class SystemLog : BaseEntity
+    {
+        public string MachineName { get; set; }
+        public string SiteName { get; set; }
+        public DateTime Logged { get; set; }
+        public string Level { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        public string Logger { get; set; }
+        public string Properties { get; set; }
+        public string ServerName { get; set; }
+        public string Port { get; set; }
+        public string Url { get; set; }
+        public bool Https { get; set; }
+        public string ServerAddress { get; set; }
+        public string RemoteAddress { get; set; }
+        public string CallSite { get; set; }
+        public string Exception { get; set; }
+    }
+}
