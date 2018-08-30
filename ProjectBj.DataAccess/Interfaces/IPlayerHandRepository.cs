c8e81b3df87559a63ed9479e4865381cb7ceb7a3
Igor Komin <ik.kture@gmail.com>
2018-08-30 12:53:20 +0300

    Initial commit
    Add PlayerHandRepository

---
+++ ProjectBj.DataAccess/Interfaces/IPlayerHandRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBj.DataAccess.Interfaces
+{
+    public interface IPlayerHandRepository
+    {
+        Task AddCard(int playerId, int cardId, int sessionId);
+    }
+}
---
+++ ProjectBj.DataAccess/Repositories/PlayerHandRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using ProjectBj.DataAccess.ExceptionHandlers;
+using ProjectBj.DataAccess.Interfaces;
+using ProjectBj.Entities;
+using ProjectBj.Logger;
+
+namespace ProjectBj.DataAccess.Repositories
+{
+    public class PlayerHandRepository : IPlayerHandRepository
+    {
+        private string _connectionString;
+
+        public PlayerHandRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task AddCard(int playerId, int cardId, int sessionId)
+        {
+            try
+            {
+                PlayerHand playerHand = new PlayerHand
+                {
+                    CardId = cardId,
+                    PlayerId = playerId,
+                    SessionId = sessionId
+                };
+                using (IDbConnection db = new SqlConnection(_connectionString))
+                {
+                    await db.InsertAsync(playerHand);
+                }
+            }
+            catch (SqlException exception)
+            {
+                Log.Error(exception.Message);
+                throw new DataSourceException(exception.Message, exception);
+            }
+        }
+    }
+}
