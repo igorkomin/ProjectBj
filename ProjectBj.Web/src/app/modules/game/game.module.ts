3832bb524fa66a1ba1d0aed64e3f67bbc10d15da
Igor Komin <ik.kture@gmail.com>
2018-08-31 14:22:15 +0300

    Add shared module

--- ProjectBj.Web/src/app/modules/game/game.module.ts
+++ ProjectBj.Web/src/app/modules/game/game.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { SharedModule } from './../shared.module';
 import { GameRoutingModule } from './game-routing.module';
 import { GameComponent } from '../../components/game.component';
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     GameRoutingModule
     ],
     declarations: [GameComponent]
--- ProjectBj.Web/src/app/modules/login/login.module.ts
+++ ProjectBj.Web/src/app/modules/login/login.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { SharedModule } from './../shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from '../../components/login.component';
 
 @NgModule({
   imports: [
-    CommonModule,
       LoginRoutingModule,
-      FormsModule
+      SharedModule
    ],
     declarations: [LoginComponent]
 })
