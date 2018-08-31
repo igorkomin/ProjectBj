1b359367d1fe35fb3e8dfdbac79599af3743007d
Igor Komin <ik.kture@gmail.com>
2018-08-31 17:54:40 +0300

    Refactor code

--- ProjectBj.DataAccess/Interfaces/ICardRepository.cs
+++ ProjectBj.DataAccess/Interfaces/ICardRepository.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using ProjectBj.Entities;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using ProjectBj.Entities;
 
 namespace ProjectBj.DataAccess.Interfaces
 {
--- ProjectBj.DataAccess/Interfaces/IGameSessionRepository.cs
+++ ProjectBj.DataAccess/Interfaces/IGameSessionRepository.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using ProjectBj.Entities;
 using System.Threading.Tasks;
-using ProjectBj.Entities;
 
 namespace ProjectBj.DataAccess.Interfaces
 {
