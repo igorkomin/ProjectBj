ce76046aecb7c7d334069a67f1bb0053d6c9b35c
Igor Komin <ik.kture@gmail.com>
2018-08-31 15:58:50 +0300

    Format code

--- ProjectBj.Web/src/app/modules/shared.module.ts
+++ ProjectBj.Web/src/app/modules/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [
+    imports: [
         CommonModule
     ],
     exports: [
