4aef15242a91b12b896de9b2f914a1da87621754
Igor Komin <ik.kture@gmail.com>
2018-09-06 10:45:24 +0300

    Initial commit
    OWIN configuration

---
+++ ProjectBj.Web/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+using ProjectBj.Web.Configs;
+
+[assembly: OwinStartup(typeof(ProjectBj.Web.Startup))]
+
+namespace ProjectBj.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var container = AutofacConfig.ConfigureContainer();
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+        }
+    }
+}
