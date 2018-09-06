8f28294b4bf22ae5f9762224d862b69e13375969
Igor Komin <ik.kture@gmail.com>
2018-09-06 12:54:52 +0300

    Remove unnecessary usings

--- ProjectBj.BusinessLogic/Services/SystemLogService.cs
+++ ProjectBj.BusinessLogic/Services/SystemLogService.cs
@@ -1,8 +1,6 @@
 ﻿using ProjectBj.BusinessLogic.Interfaces;
 using ProjectBj.DataAccess.Interfaces;
-using ProjectBj.Logger;
 using ProjectBj.ViewModels.Logs;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
