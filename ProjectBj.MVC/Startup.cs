a1d22f9f4f0be0c730e5b98954ee777ce604a0f7
Igor Komin <ik.kture@gmail.com>
2018-09-07 14:58:56 +0300

    Initial commit

---
+++ ProjectBj.MVC/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+using ProjectBj.MVC.Configs;
+
+[assembly: OwinStartup(typeof(ProjectBj.MVC.Startup))]
+
+namespace ProjectBj.MVC
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var container = AutofacConfig.ConfigureContainer();
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+        }
+    }
+}
