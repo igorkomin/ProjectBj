fdb394fa08905ff2ca87b7ff370169381fc3202f
Igor Komin <ik.kture@gmail.com>
2018-09-11 09:12:45 +0300

    fix eqeqeq rule

--- ProjectBj.MVC/Scripts/bj/game.js
+++ ProjectBj.MVC/Scripts/bj/game.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     $("#inc-bots-btn").click(function () {
         var number = $("#bots-number").val();
-        if (number == 5) {
+        if (number === 5) {
             enableBotDecrease();
             return;
         }
@@ -205,7 +205,7 @@ function showBotsData(game) {
 
 function checkGameStatus(game) {
     switchToMainMenu();
-    if (game && game.player.gameResult == 0) {
+    if (game && game.player.gameResult === 0) {
         switchToGameControls();
     }
 }
