15a62a744b59701a804a1d5c8b24cb16a8a6aaf6
Igor Komin <ik.kture@gmail.com>
2018-09-12 10:46:44 +0300

    Format code

--- ProjectBj.Web/src/app/app.component.ts
+++ ProjectBj.Web/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-    styleUrls: [
-        'app.component.css',
-        '/shared/style/bootstrap.css'
-    ]
+  styleUrls: [
+      'app.component.css',
+      '/shared/style/bootstrap.css'
+  ]
 })
 export class AppComponent {
 }
