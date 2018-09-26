43743d51932169f32cee80382735f43d851d3bc3
Igor Komin <ik.kture@gmail.com>
2018-09-26 17:00:07 +0300

    Resolve issue #17

--- ProjectBj.Web/src/app/logs/logs.module.ts
+++ ProjectBj.Web/src/app/logs/logs.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { LogsRoutingModule } from 'src/app/logs/logs-routing.module';
-import { LogsComponent } from 'src/app/logs/logs.component';
+import { LogsComponent } from 'src/app/logs/logs/logs.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
