479e904605133d7902d87854cfc8e020c506279f
Igor Komin <ik.kture@gmail.com>
2018-09-28 09:06:05 +0300

    Remove requestOptions variable

--- ProjectBj.Web/src/app/history/history.service.ts
+++ ProjectBj.Web/src/app/history/history.service.ts
@@ -1,29 +1,23 @@
-﻿import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+﻿import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { History } from 'src/app/shared/models/history.model';
 
 const apiUrl = '../api/history';
-const requestOptions = {
-    headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-    })
-};
 
 @Injectable({
     providedIn: 'root'
 })
-
 export class HistoryService {
     constructor(private http: HttpClient) { }
 
     getHistory(sessionId: number): Observable<History> {
         let requestUrl = `${apiUrl}/GetGameHistory`;
-        return this.http.post<History>(requestUrl, sessionId, requestOptions);
+        return this.http.post<History>(requestUrl, sessionId);
     }
 
     getFullHistory(): Observable<History> {
         let requestUrl = `${apiUrl}/GetFullHistory`;
-        return this.http.get<History>(requestUrl, requestOptions);
+        return this.http.get<History>(requestUrl);
     }
 }
