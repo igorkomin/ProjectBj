89e4f7f150f10700de5da90a7f87329bad902dbf
Igor Komin <ik.kture@gmail.com>
2018-09-28 09:07:09 +0300

    Add Interceptor

--- ProjectBj.Web/src/app/app.module.ts
+++ ProjectBj.Web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApiInterceptor } from 'src/app/shared/api.interceptor';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -21,7 +22,9 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
         GridModule,
         BrowserAnimationsModule
     ],
-    providers: [],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
