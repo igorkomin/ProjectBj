dd83a46091df3d0bb472edda1168532392cb1db1
Igor Komin <ik.kture@gmail.com>
2018-09-28 11:44:31 +0300

    Rename ApiInterceptor to HttpErrorInterceptor

---
+++ ProjectBj.Web/src/app/shared/http-error.interceptor.ts
@@ -0,0 +1,25 @@
+﻿import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+    intercept(
+        request: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(
+            catchError((error: HttpErrorResponse) => {
+                let errorMessage = '';
+                if (error.error instanceof ErrorEvent) {
+                    errorMessage = `Error: ${error.error.message}`;
+                }
+                else {
+                    errorMessage = `Error Code: ${error.status},  Message: ${error.message}`;
+                }
+                return throwError(errorMessage);
+            })
+        );
+    }
+}
