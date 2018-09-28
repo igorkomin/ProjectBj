e96ddf8f562cfb7bc26acb1e1dffb93a8029ab75
Igor Komin <ik.kture@gmail.com>
2018-09-28 09:05:31 +0300

    Remove requestOptions variable

--- ProjectBj.Web/src/app/game/game.service.ts
+++ ProjectBj.Web/src/app/game/game.service.ts
@@ -1,4 +1,4 @@
-﻿import { HttpClient, HttpHeaders } from '@angular/common/http';
+﻿import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameRequest } from '../shared/models/game-request.model';
@@ -6,46 +6,40 @@ import { Game } from '../shared/models/game.model';
 import { NewGameRequest } from '../shared/models/new-game-request.model';
 
 const apiUrl = '../api/game';
-const requestOptions = {
-    headers: new HttpHeaders({
-        'Content-Type' : 'application/json'
-    })
-}
 
 @Injectable({
     providedIn: 'root'
 })
-
 export class GameService {
     constructor(private http: HttpClient) { }
 
     newGame(request: NewGameRequest): Observable<Game> {
         let requestUrl = `${apiUrl}/Start`;
-        return this.http.post<Game>(requestUrl, request, requestOptions);
+        return this.http.post<Game>(requestUrl, request);
     }
 
     loadGame(request: NewGameRequest): Observable<Game> {
         let requestUrl = `${apiUrl}/Load`;
-        return this.http.post<Game>(requestUrl, request, requestOptions);
+        return this.http.post<Game>(requestUrl, request);
     }
 
     hit(request: GameRequest): Observable<Game> {
         let requestUrl = `${apiUrl}/Hit`;
-        return this.http.post<Game>(requestUrl, request, requestOptions);
+        return this.http.post<Game>(requestUrl, request);
     }
 
     stand(request: GameRequest): Observable<Game> {
         let requestUrl = `${apiUrl}/Stand`;
-        return this.http.post<Game>(requestUrl, request, requestOptions);
+        return this.http.post<Game>(requestUrl, request);
     }
 
     double(request: GameRequest): Observable<Game> {
         let requestUrl = `${apiUrl}/Double`;
-        return this.http.post<Game>(requestUrl, request, requestOptions);
+        return this.http.post<Game>(requestUrl, request);
     }
 
     surrender(request: GameRequest): Observable<Game> {
         let requestUrl = `${apiUrl}/Surrender`;
-        return this.http.post<Game>(requestUrl, request, requestOptions);
+        return this.http.post<Game>(requestUrl, request);
     }
 }
