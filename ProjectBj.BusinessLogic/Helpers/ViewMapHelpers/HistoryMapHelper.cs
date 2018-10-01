f63c3f452c3a0a3ec7673c7a0df0e4da04f2f207
Igor Komin <ik.kture@gmail.com>
2018-10-01 12:12:41 +0300

    Use AutoMapper in HistoryMapHelper

--- ProjectBj.BusinessLogic/Helpers/ViewMapHelpers/HistoryMapHelper.cs
+++ ProjectBj.BusinessLogic/Helpers/ViewMapHelpers/HistoryMapHelper.cs
@@ -1,4 +1,5 @@
-﻿using ProjectBj.Entities;
+﻿using AutoMapper;
+using ProjectBj.Entities;
 using ProjectBj.ViewModels.History;
 using System.Collections.Generic;
 
@@ -8,16 +9,11 @@ namespace ProjectBj.BusinessLogic.Helpers.ViewMapHelpers
     {
         public static IEnumerable<GetGameHistoryHistoryView> GetGameHistoryView(IEnumerable<History> history)
         {
+            Mapper.Initialize(cfg => cfg.CreateMap<History, GetGameHistoryHistoryView>());
             var gameHistoryViews = new List<GetGameHistoryHistoryView>();
             foreach (var entry in history)
             {
-                var gameHistoryView = new GetGameHistoryHistoryView
-                {
-                    SessionId = entry.SessionId,
-                    Time = entry.CreationDate,
-                    PlayerName = entry.PlayerName,
-                    Event = entry.Event
-                };
+                GetGameHistoryHistoryView gameHistoryView = Mapper.Map<GetGameHistoryHistoryView>(entry);
                 gameHistoryViews.Add(gameHistoryView); 
             }
             return gameHistoryViews;
@@ -25,16 +21,11 @@ namespace ProjectBj.BusinessLogic.Helpers.ViewMapHelpers
 
         public static IEnumerable<GetFullHistoryHistoryView> GetFullHistoryView(IEnumerable<History> history)
         {
+            Mapper.Initialize(cfg => cfg.CreateMap<History, GetFullHistoryHistoryView>());
             var fullHistoryViews = new List<GetFullHistoryHistoryView>();
             foreach (var entry in history)
             {
-                var fullHistoryView = new GetFullHistoryHistoryView
-                {
-                    SessionId = entry.SessionId,
-                    Time = entry.CreationDate,
-                    PlayerName = entry.PlayerName,
-                    Event = entry.Event
-                };
+                GetFullHistoryHistoryView fullHistoryView = Mapper.Map<GetFullHistoryHistoryView>(entry);
                 fullHistoryViews.Add(fullHistoryView);
             }
             return fullHistoryViews;
