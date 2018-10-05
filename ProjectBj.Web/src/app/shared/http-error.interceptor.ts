6692f4fed5208af4d98be35498d76d650e1a8b36
Igor Komin <ik.kture@gmail.com>
2018-10-05 12:11:21 +0300

    Update HttpErrorInterceptor to redirect to error page

--- ProjectBj.Web/src/app/shared/http-error.interceptor.ts
+++ ProjectBj.Web/src/app/shared/http-error.interceptor.ts
@@ -2,22 +2,22 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
+    constructor(
+        private router: Router
+    ) { }
     intercept(
         request: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(
             catchError((error: HttpErrorResponse) => {
-                let errorMessage = '';
-                if (error instanceof ErrorEvent) {
-                    errorMessage = `Error: ${error.message}`;
-                }
-                else {
-                    errorMessage = `Error Code: ${error.status},  Message: ${error.message}`;
-                }
+                let errorMessage = `Error Code: ${error.status},  Message: ${error.message}`;
+                this.router.navigate(['/error', { 'code': error.status }]);
+
                 return throwError(errorMessage);
             })
         );
