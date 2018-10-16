c7f6c6f3d73813ecc12ef3bde51f87213f149858
Igor Komin <ik.kture@gmail.com>
2018-10-16 18:22:25 +0300

    Update Angular folder structure.

--- ProjectBj.Web/src/app/game/game/game.component.ts
+++ ProjectBj.Web/src/app/game/game/game.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GameService } from 'src/app/game/shared/game.service';
-import { GetGameHistoryHistoryView } from 'src/app/game/shared/models/get-game-history-history-view.model';
-import { RequestGameView } from 'src/app/game/shared/models/request-game-view.model';
-import { RequestNewGameView } from 'src/app/game/shared/models/request-new-game-view.model';
-import { ResponseDoubleGameView } from 'src/app/game/shared/models/response-double-game-view.model';
-import { ResponseHitGameView } from 'src/app/game/shared/models/response-hit-game-view.model';
-import { ResponseLoadGameView } from 'src/app/game/shared/models/response-load-game-view.model';
-import { ResponseStandGameView } from 'src/app/game/shared/models/response-stand-game-view.model';
-import { ResponseStartGameView } from 'src/app/game/shared/models/response-start-game-view.model';
-import { ResponseSurrenderGameView } from 'src/app/game/shared/models/response-surrender-game-view.model';
+import { GameService } from 'src/app/shared/services/game.service';
+import { GetGameHistoryHistoryView } from 'src/app/shared/models/get-game-history-history-view.model';
+import { RequestGameView } from 'src/app/shared/models/request-game-view.model';
+import { RequestNewGameView } from 'src/app/shared/models/request-new-game-view.model';
+import { ResponseDoubleGameView } from 'src/app/shared/models/response-double-game-view.model';
+import { ResponseHitGameView } from 'src/app/shared/models/response-hit-game-view.model';
+import { ResponseLoadGameView } from 'src/app/shared/models/response-load-game-view.model';
+import { ResponseStandGameView } from 'src/app/shared/models/response-stand-game-view.model';
+import { ResponseStartGameView } from 'src/app/shared/models/response-start-game-view.model';
+import { ResponseSurrenderGameView } from 'src/app/shared/models/response-surrender-game-view.model';
 
 @Component({
     selector: 'app-game',
