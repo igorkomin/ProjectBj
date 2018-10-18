04f3fc44fb970761ea05f13bbbde8e5477a3609d
Igor Komin <ik.kture@gmail.com>
2018-10-18 13:30:29 +0300

    Sort methods in EnumHelper

--- ProjectBj.BusinessLogic/Helpers/EnumHelper.cs
+++ ProjectBj.BusinessLogic/Helpers/EnumHelper.cs
@@ -7,17 +7,17 @@ namespace ProjectBj.BusinessLogic.Helpers
 {
     public static class EnumHelper
     {
-        public static string GetEnumDescription(Enum value)
-        {
-            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
-            DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes[0].Description;
-        }
-
         public static string GetCardRankName(CardRank rank)
         {
             string rankName = GetEnumDescription(rank);
             return rankName;
+        }
+
+        private static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes[0].Description;
         }
     }
 }
