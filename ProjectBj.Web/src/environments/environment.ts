86b7550476b78c65186f08911b79db37d0b2bd99
Igor Komin <ik.kture@gmail.com>
2018-10-18 15:26:36 +0300

    Add slash to api urls

--- ProjectBj.Web/src/environments/environment.ts
+++ ProjectBj.Web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    gameApiUrl: 'http://localhost:54890/api/game',
-    historyApiUrl: 'http://localhost:54890/api/history',
-    logApiUrl: 'http://localhost:54890/api/log'
+    gameApiUrl: 'http://localhost:54890/api/game/',
+    historyApiUrl: 'http://localhost:54890/api/history/',
+    logApiUrl: 'http://localhost:54890/api/log/'
 };
