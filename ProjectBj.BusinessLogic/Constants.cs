aca49f713ca06f2541a46d05984d51b08a5512df
Igor Komin <ik.kture@gmail.com>
2018-10-18 13:07:51 +0300

    Rename ValueHelper to Constants, move to root folder

---
+++ ProjectBj.BusinessLogic/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectBj.BusinessLogic
+{
+    public static class Constants
+    {
+        public const int AceCardValue = 11;
+        public const int AceDelta = 10;
+        public const int BlackjackValue = 21;
+        public const int FaceCardValue = 10;
+        public const int MinimumDealerHandValue = 17;
+        public const int MinimumBotHandValue = 18;
+    }
+}
