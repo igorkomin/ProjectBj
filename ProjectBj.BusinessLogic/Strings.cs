675e05bd605869e8475d4333fa27d61a58167c62
Igor Komin <ik.kture@gmail.com>
2018-10-18 13:07:28 +0300

    Rename StringHelper to Strings, move to root folder

---
+++ ProjectBj.BusinessLogic/Strings.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectBj.BusinessLogic
+{
+    public static class Strings
+    {
+        public const string DealerName = "Dealer";
+        public const string ChoseToHitMessage = "chose to hit";
+        public const string ChoseToStandMessage = "chose to stand";
+        public const string ChoseToDoubleMessage = "chose to double";
+        public const string ChoseToSurrenderMessage = "chose to surrender";
+        public const string NameReservedMessage = "This name is reserved";
+        public const string NoGameToLoadMessage = "No game to load";
+        public const string RandomCardsExceptionMessage = "count must be more then 0";
+        public const string BotsNumberMustBePositiveMessage = "botsNumber must be 0 or positive";
+
+        public static string GetPlayerTakesCardMessage(string cardRank, string cardSuit)
+        {
+            return $"takes {cardRank} of {cardSuit}";
+        }
+    }
+}
