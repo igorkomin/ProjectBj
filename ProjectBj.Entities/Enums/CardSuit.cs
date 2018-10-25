98b6add33442027a013e8c199a1d3810a22e7ef5
Igor Komin <ik.kture@gmail.com>
2018-10-25 13:58:53 +0300

    Remove unused descriptions from CardSuit

--- ProjectBj.Entities/Enums/CardSuit.cs
+++ ProjectBj.Entities/Enums/CardSuit.cs
@@ -1,17 +1,11 @@
-﻿using System.ComponentModel;
-
-namespace ProjectBj.Entities.Enums
+﻿namespace ProjectBj.Entities.Enums
 {
     public enum CardSuit
     {
         None = 0,
-        [Description("spades")]
         Spades = 1,
-        [Description("clubs")]
         Clubs = 2,
-        [Description("hearts")]
         Hearts = 3,
-        [Description("diams")]
         Diamonds = 4
     }
 }
