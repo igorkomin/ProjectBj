f4ab18011de4b9382b66ce59c1fb191e56a4caac
Igor Komin <ik.kture@gmail.com>
2018-10-31 12:53:44 +0200

    Remove unused code

--- ProjectBj.DataAccess/Repositories/HistoryRepository.cs
+++ ProjectBj.DataAccess/Repositories/HistoryRepository.cs
@@ -1,30 +1,12 @@
-﻿using Dapper;
-using ProjectBj.DataAccess.Repositories.Interfaces;
+﻿using ProjectBj.DataAccess.Repositories.Interfaces;
 using ProjectBj.Entities;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Threading.Tasks;
 
 namespace ProjectBj.DataAccess.Repositories
 {
     public class HistoryRepository : BaseRepository<History>, IHistoryRepository
     {
-        private readonly string _connectionString;
-
         public HistoryRepository(string connectionString): base(connectionString)
         {
-            _connectionString = connectionString;
-        }
-
-        public async Task<IEnumerable<History>> GetBySessionId(long sessionId)
-        {
-            using (IDbConnection db = new SqlConnection(_connectionString))
-            {
-                string sqlQuery = @"SELECT * FROM History WHERE SessionId = @sessionId";
-                IEnumerable<History> history = await db.QueryAsync<History>(sqlQuery, new { sessionId });
-                return history;
-            }
         }
     }
 }
